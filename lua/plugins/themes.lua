11b28205bb457150ec907a7d0b49c5ded39d7c23
logg1s <lrng159@gmail.com>
2025-02-05 08:55:59 +0700

    config transparent background theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,6 +1,6 @@
-  return {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  }
+return {
+  "LazyVim/LazyVim",
+  opts = {
+    colorscheme = "catppuccin",
+  },
+}
