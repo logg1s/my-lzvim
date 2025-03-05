3752c3d6c2b4918fd4a45fbe470e7f45758cb556
l0gg1s <lrng159@gmail.com>
2025-03-05 16:25:45 +0700

    vtsls comeback - stronger when upgrade typescript project version

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,15 @@
+return {
+  -- "folke/noice.nvim",
+  -- event = "VeryLazy",
+  -- opts = {
+  --   routes = {
+  --     {
+  --       filter = {
+  --         event = "notify",
+  --         find = "Request textDocument/inlayHint failed",
+  --       },
+  --       opts = { skip = true },
+  --     },
+  --   },
+  -- },
+}
