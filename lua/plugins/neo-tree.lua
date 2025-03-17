aed21a70bca0a320418256fdd12660999fbed276
logg1s <lrng159@gmail.com>
2025-03-17 11:23:10 +0700

    revert to use fzf-lua

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        -- watcher on windows
+        use_libuv_file_watcher = false,
+      },
+    },
+  },
+}
