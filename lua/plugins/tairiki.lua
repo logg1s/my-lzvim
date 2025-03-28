c90792de81172268fc4d213cd4de9b791efc8f00
logg1s <lrng159@gmail.com>
2025-03-28 12:04:28 +0700

    add new themes

---
+++ lua/plugins/tairiki.lua
@@ -0,0 +1,8 @@
+return {
+  "deparr/tairiki.nvim",
+  lazy = false,
+  priority = 1000, -- recommended if you use tairiki as your default theme
+  opts = {
+    transparent = true, -- don't set background colors
+  },
+}
